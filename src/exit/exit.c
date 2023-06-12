9f9d6b09885ba34ab4bfb60e925d1969132f5618
johnnymosby <72984340+johnnymosby@users.noreply.github.com>
2023-06-12 13:29:09 +0200

    freeing all arguments with one free call instead of several calls

--- src/exit/exit.c
+++ src/exit/exit.c
@@ -6,7 +6,7 @@
 /*   By: rbasyrov <rbasyrov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 14:34:32 by rbasyrov          #+#    #+#             */
-/*   Updated: 2023/06/12 11:39:42 by rbasyrov         ###   ########.fr       */
+/*   Updated: 2023/06/12 13:28:54 by rbasyrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ void	free_cmd_tbl(t_cmd_tbl *cmd_tbl)
 	if (cmd_tbl->cmd != NULL)
 		free(cmd_tbl->cmd);
 	if (cmd_tbl->args != NULL)
-		free_str_array(&cmd_tbl->args, cmd_tbl->n_args);
+		free_if_not_null((void **)&cmd_tbl->args);
 	if (cmd_tbl->in >= 0)
 	{
 		close(cmd_tbl->in);
