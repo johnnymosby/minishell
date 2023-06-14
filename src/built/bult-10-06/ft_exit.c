0c1140ccdf1f46536ba598a9aca46307f074145e
Marks Ruziboev <112407232+marksruziboev@users.noreply.github.com>
2023-06-14 09:18:18 +0200

    exit modified

--- src/built/bult-10-06/ft_exit.c
+++ src/built/bult-10-06/ft_exit.c
@@ -12,28 +12,26 @@
 
 #include "builts.h"
 
-int ft_exit(t_shell *cmd, char *code)
+int ft_exit(t_list *cmd, int *exit_code)
 {
-    t_cmd_tbl	*cmd_tbls;	
-    char command; 
-	long	status[2];
+    t_shell	*cmd_tbls;	 
+	int	status[2];
 
-	command = cmd->content;
-    
-	*is_exit = !cmd->next;
+	current = !cmd->content;
+	if (!cmd_tbls->cmd_tbls || !cmd_tbls->cmd_tbls[1])
+		return (0);
+	*exit_code = !cmd->next;
 	if (*is_exit)
 		ft_putstr_fd("exit\n", 2);
-	if (!command->full_cmd || !command->full_cmd[1])
-		return (0);
-	status[1] = ft_atoi2(command->full_cmd[1], &status[0]);
+	status[1] = ft_atoi(cmd_tbls->cmd_tbls[1], &status[0]);
 	if (status[1] == -1)
 	{
 		ft_putstr_fd("minishell: exit: ", 2);
-		ft_putstr_fd(command->full_cmd[1], 2);
+		ft_putstr_fd(cmd_tbls->cmd_tbls[1], 2);
 		ft_putstr_fd(": numeric argument required\n", 2);
 		return (255);
 	}
-	else if (command->full_cmd[2])
+	else if (cmd_tbls->cmd_tbls[2])
 	{
 		*is_exit = 0;
 		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
