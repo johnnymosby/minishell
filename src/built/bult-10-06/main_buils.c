4d8a9da667cef6321ed6791194baf4b0105224a5
maruzibo <marx1987@mail.ru>
2023-06-16 16:06:38 +0200

    cleaned builts main

--- src/built/bult-10-06/main_buils.c
+++ src/built/bult-10-06/main_buils.c
@@ -6,7 +6,7 @@
 /*   By: maruzibo <maruzibo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 13:15:41 by maruzibo          #+#    #+#             */
-/*   Updated: 2023/06/11 13:15:44 by maruzibo         ###   ########.fr       */
+/*   Updated: 2023/06/16 15:52:42 by maruzibo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,125 +16,32 @@
 
 extern int status;
 
-int ft_built(t_prompt *prompt, t_list *cmd, int *is_exit, int n)
+int ft_built(t_shell *hell)
 {
-	char	**a;
-
-	while (cmd)
+	t_cmd_tbl	*cmd;
+	int	n[2];
+	
+	n[0] = -1;
+	n[1] = 0;
+	while (++n[0] < hell->n_cmd_args)
 	{
-		a = ((t_mini *)cmd->content)->full_cmd;
-		n = 0;
-		if (a)
-			n = ft_strlen(*a);
-		if (a && !ft_strncmp(*a, "exit", n) && n == 4)
-			g_status = mini_exit(cmd, is_exit);
-		else if (!cmd->next && a && !ft_strncmp(*a, "cd", n) && n == 2)
-			g_status = mini_cd(prompt);
-		else if (!cmd->next && a && !ft_strncmp(*a, "export", n) && n == 6)
-			g_status = mini_export(prompt);
-		else if (!cmd->next && a && !ft_strncmp(*a, "unset", n) && n == 5)
-			g_status = mini_unset(prompt);
+		cmd = hell->cmd_tbls;
+		if (cmd->cmd)
+			n[1] = ft_strlen(*cmd->cmd);
+		if (cmd->cmd && n[1] == 4 && !ft_strncmp(*cmd->cmd, "exit", n[1]))
+			status = ft_exit(hell, is_exit);
+		else if (n[0] ==  && n[1] == 2 && a && !ft_strncmp(*cmd->cmd, "cd", n[1]))
+			status = ft_cd(hell);
+		else if (!cmd->next && n[1] == 6 && a && !ft_strncmp(*cmd->cmd, "export", n[1]) )
+			status = ft_export(hell);
+		else if (!cmd->next && n[1] == 5 && a && !ft_strncmp(*cmd->cmd, "unset", n[1]) )
+			status = ft_unset(hell);
 		else
 		{
 			signal(SIGINT, SIG_IGN);
 			signal(SIGQUIT, SIG_IGN);
-			exec_cmd(prompt, cmd);
-		}
-		cmd = cmd->next;
-	}
-	return (g_status);
-}
-
-int	is_builtin(t_mini *n)
-{
-	int		l;
-
-	if (!n->full_cmd)
-		return (0);
-	if ((n->full_cmd && ft_strchr(*n->full_cmd, '/')) || (n->full_path && \
-		ft_strchr(n->full_path, '/')))
-		return (0);
-	l = ft_strlen(*n->full_cmd);
-	if (!ft_strncmp(*n->full_cmd, "pwd", l) && l == 3)
-		return (1);
-	if (!ft_strncmp(*n->full_cmd, "env", l) && l == 3)
-		return (1);
-	if (!ft_strncmp(*n->full_cmd, "cd", l) && l == 2)
-		return (1);
-	if (!ft_strncmp(*n->full_cmd, "export", l) && l == 6)
-		return (1);
-	if (!ft_strncmp(*n->full_cmd, "unset", l) && l == 5)
-		return (1);
-	if (!ft_strncmp(*n->full_cmd, "echo", l) && l == 4)
-		return (1);
-	if (!ft_strncmp(*n->full_cmd, "exit", l) && l == 4)
-		return (1);
-	return (0);
-}
-
-int	mini_cd(t_prompt *p)
-{
-	char	**str[2];
-	char	*aux;
-
-	g_status = 0;
-	str[0] = ((t_mini *)p->cmds->content)->full_cmd;
-	aux = mini_getenv("HOME", p->envp, 4);
-	if (!aux)
-		aux = ft_strdup("");
-	str[1] = ft_extend_matrix(NULL, aux);
-	free(aux);
-	aux = getcwd(NULL, 0);
-	str[1] = ft_extend_matrix(str[1], aux);
-	free(aux);
-	cd_error(str);
-	if (!g_status)
-		p->envp = mini_setenv("OLDPWD", str[1][1], p->envp, 6);
-	aux = getcwd(NULL, 0);
-	if (!aux)
-		aux = ft_strdup("");
-	str[1] = ft_extend_matrix(str[1], aux);
-	free(aux);
-	p->envp = mini_setenv("PWD", str[1][2], p->envp, 3);
-	ft_free_matrix(&str[1]);
-	return (g_status);
-}
-
-int	ft_pwd(void)
-{
-	char	*path;
-
-	path = getcwd(NULL, 0);
-	ft_putendl_fd(path, 1);
-	free(buf);
-	return (0);
-}
-
-int	ft_echo(t_list *cmd)
-{
-	int		newline;
-	int		i[2];
-	char	**argv;
-	t_mini	*node;
-
-	i[0] = 0;
-	i[1] = 0;
-	newline = 1;
-	node = cmd->content;
-	argv = node->full_cmd;
-	while (argv && argv[++i[0]])
-	{
-		if (!i[1] && !ft_strncmp(argv[i[0]], "-n", 2) && \
-			(ft_countchar(argv[i[0]], 'n') == \
-			(int)(ft_strlen(argv[i[0]]) - 1)))
-			newline = 0;
-		else
-		{
-			i[1] = 1;
-			ft_putstr_fd(argv[i[0]], 1);
-			if (argv[i[0] + 1])
-				ft_putchar_fd(' ', 1);
+			exec_cmd(hell);
 		}
 	}
-	return (write(1, "\n", newline) == 2);
+	return (status);
 }
