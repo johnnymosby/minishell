c375b23fd2b0dee5f4f7a85eb871056dcc9ed12a
Ruslan Basyrov <rbasyrov@c4r4p3.42vienna.com>
2023-06-28 18:50:17 +0200

    fixed wrong piping (closing both ends instead of only one after executing a command)

--- src/signals/check_signals.c
+++ src/signals/check_signals.c
@@ -6,7 +6,7 @@
 /*   By: rbasyrov <rbasyrov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 16:17:40 by rbasyrov          #+#    #+#             */
-/*   Updated: 2023/06/25 16:41:47 by rbasyrov         ###   ########.fr       */
+/*   Updated: 2023/06/28 18:49:19 by rbasyrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,7 @@ static void	handle_sigint(int sig)
 		else if (g_status == CMD_NOSIG || g_status == CMD_SIG)
 			g_status = CMD_SIG;
 		if (g_status == HEREDOC)
-		{
 			ioctl(STDIN_FILENO, TIOCSTI, "\n");
-		}
 		else
 			write(1, "\n", 2);
 		rl_replace_line("", 0);
